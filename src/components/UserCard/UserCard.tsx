0ff405b112490bcdbd091ce4f3ea203d67200e12
alexeiVinnichek <alexei.vinnichek@mail.ru>
2024-11-19 12:11:49 +0300

    added new copmonent userCard

---
+++ src/components/UserCard/UserCard.tsx
@@ -0,0 +1,73 @@
+import { Card, CardHeader, Avatar, Divider, CardActions, IconButton } from "@mui/material";
+import { ICustomUser } from "../../types/user.interface";
+import LogoutIcon from '@mui/icons-material/Logout';
+import EditIcon from '@mui/icons-material/Edit';
+import AdminPanelSettingsIcon from '@mui/icons-material/AdminPanelSettings';
+import { logOutAuthorisedUser } from "../../features/authorisedUser/authorisedUserSlice";
+import { useDispatch } from "react-redux";
+
+interface IUserCard {
+    user: ICustomUser;
+};
+
+const UserCard = ({ user }: IUserCard) => {
+    const dispatch = useDispatch();
+
+    const handleClickLogOut = () => {
+        dispatch(logOutAuthorisedUser());
+    };
+
+    const handleEdit = () => {
+
+    };
+
+    return (
+        <Card
+            variant="outlined"
+            sx={{
+                width: '90%',
+                boxShadow: '0 5px 20px #ABB2B9;',
+                borderRadius: '22px'
+            }}
+            className="relative"
+        >
+            <CardHeader
+                avatar={
+                    <Avatar
+                        alt={user.username}
+                        src="/assets/avatar.png"
+                        className="!w-20 !h-20"
+                    />
+                }
+                title={
+                    <h3 className="text-3xl font-bold text-blue-700">
+                        {user.name}
+                        {user.isAdmin && <AdminPanelSettingsIcon className=" text-admin-icon" />}
+                    </h3>}
+                subheader={<p className="text-sm text-gray-500">{user.username}</p>}
+            />
+
+            <Divider />
+
+            <CardActions>
+                <div className="w-full flex flex-row justify-between items-center">
+                    <IconButton
+                        title='Edit'
+                        onClick={handleEdit}
+                    >
+                        <EditIcon className="text-basic-color" />
+                    </IconButton>
+                    <IconButton
+                        title='Log out'
+                        onClick={handleClickLogOut}
+                    >
+                        <LogoutIcon className="text-basic-color" />
+                    </IconButton>
+                </div>
+
+            </CardActions>
+        </Card>
+    );
+};
+
+export { UserCard };
--- src/pages/ProfilePage/ProfilePage.tsx
+++ src/pages/ProfilePage/ProfilePage.tsx
@@ -1,70 +1,13 @@
-import { Card, Divider, CardActions, IconButton, Avatar, CardHeader } from "@mui/material";
-import { useDispatch, useSelector } from "react-redux";
-import { logOutAuthorisedUser } from "../../features/authorisedUser/authorisedUserSlice";
-import LogoutIcon from '@mui/icons-material/Logout';
+import { useSelector } from "react-redux";
 import { selectIsAuthorisedUser } from "../../features/authorisedUser/authorisedUserSelectors";
-import EditIcon from '@mui/icons-material/Edit';
-import AdminPanelSettingsIcon from '@mui/icons-material/AdminPanelSettings';
+import { UserCard } from "../../components/UserCard/UserCard";
 
 const ProfilePage = () => {
     const user = useSelector(selectIsAuthorisedUser);
-    const dispatch = useDispatch();
-
-    const handleClickLogOut = () => {
-        dispatch(logOutAuthorisedUser());
-    };
-
-    const handleEdit = () => {
-
-    };
-
+   
     return (
-        <Card
-            variant="outlined"
-            sx={{
-                width: '90%',
-                boxShadow: '0 5px 20px #ABB2B9;',
-                borderRadius: '22px'
-            }}
-            className="relative"
-        >
-            <CardHeader
-                avatar={
-                    <Avatar
-                        alt={user.username}
-                        src="/assets/avatar.png"
-                        className="!w-20 !h-20"
-                    />
-                }
-                title={
-                    <h3 className="text-3xl font-bold text-blue-700">
-                        {user.name}
-                        {user.isAdmin && <AdminPanelSettingsIcon className=" text-admin-icon" />}
-                    </h3>}
-                subheader={<p className="text-sm text-gray-500">{user.username}</p>}
-            />
-
-            <Divider />
-
-            <CardActions>
-                <div className="w-full flex flex-row justify-between items-center">
-                    <IconButton
-                        title='Edit'
-                        onClick={handleEdit}
-                    >
-                        <EditIcon className="text-basic-color" />
-                    </IconButton>
-                    <IconButton
-                        title='Log out'
-                        onClick={handleClickLogOut}
-                    >
-                        <LogoutIcon className="text-basic-color" />
-                    </IconButton>
-                </div>
-
-            </CardActions>
-        </Card>
+        <UserCard user={user}/>
     );
 };
 
-export { ProfilePage } 
+export { ProfilePage };
