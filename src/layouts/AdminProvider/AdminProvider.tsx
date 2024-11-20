f48cd1a0e037aef418a29d8082818fd7d85ef354
alexeiVinnichek <alexei.vinnichek@mail.ru>
2024-11-20 10:50:39 +0300

    router changes

--- src/layouts/AdminProvider/AdminProvider.tsx
+++ src/layouts/AdminProvider/AdminProvider.tsx
@@ -1,8 +1,6 @@
 import { Navigate, useLocation } from "react-router-dom";
 import { ROUTES } from "../../router/routes";
 import { useCurrentUser } from "../../hooks/useCurrentUser";
-import { useDispatch } from "react-redux";
-import { showErrorMessage } from "../../utils/snackMessageHelpers";
 
 interface IAuthProvider {
     children: React.ReactNode;
@@ -11,12 +9,9 @@ interface IAuthProvider {
 const AdminProvider = ({ children }: IAuthProvider) => {
     const { isAdmin } = useCurrentUser();
     const location = useLocation();
-    const dispatch = useDispatch();
 
     if (!isAdmin) {
-        dispatch(showErrorMessage(new Error('Fobidden! Only Admin can visit users!')));
-
-        return <Navigate to={ROUTES.LOGIN} state={{ from: location }} />;
+        return <Navigate to={ROUTES.PROFILE} state={{ from: location }} />;
     }
 
     return children;
